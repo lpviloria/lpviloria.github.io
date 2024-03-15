d5a48eef46c2282078d0f33a661c9a8f5817af8c
lpviloria <lpviloria@up.edu.ph>
2024-03-15 15:32:16 +0800

    feat: created server.js which contains the logic behind the server

--- exer5/server.js
+++ exer5/server.js
@@ -6,9 +6,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/add-book', (req, res) => {
-  const objValues = Object.values(req.body);
-  if(objValues.length == 4 && objValues[0] != null && objValues[1] != null && objValues[2] != null && objValues[3] != null){
+let books = []; //Empty array initializations to store data for searching purposes
+let authors = [];
+let isbns = [];
+let dates = [];
+
+app.post('/add-book', (req, res) => { //Post request to add a book
+  const objValues = Object.values(req.body); //Convert object data to array
+  books.push(objValues[0]); //Append information to their respective arrays
+  authors.push(objValues[2]);
+  isbns.push(objValues[1]);
+  dates.push(objValues[3]);
+  if(objValues.length == 4 && objValues[0] != null && objValues[1] != null && objValues[2] != null && objValues[3] != null){ //If all fields are filled out, create string to store in books.txt
     let new_string = objValues[0];
     new_string = new_string.concat(","+objValues[1]);
     new_string = new_string.concat(","+objValues[2])
@@ -28,13 +37,27 @@ app.post('/add-book', (req, res) => {
   
   })
 
-app.get('/find-by-isbn-author', (req, res) => {
-  res.send("Request successfully received.");
-  readFile("books.txt", function(err, cont) {
-    if (err)
-        throw err;
-    console.log("String"+(cont.indexOf(req.query.author)>-1 ? " " : " not ")+"found");
-  });
+app.get('/find-by-isbn-author', (req, res) => { //Get request to find a book by ISBN and author
+  for (let i = 0; i < authors.length; i++) {
+    if(authors[i] == req.query.author && isbns[i] == req.query.isbn){
+      console.log("Author: " + authors[i] + " ISBN: " + isbns[i] + " Book: " + books[i] + " Date: " + dates[i]);
+      break;
+    }
+  }
+
+  res.send("");
+
+});
+
+app.get('/find-by-author', (req, res) => { //Get request to find a book by author
+  for (let i = 0; i < authors.length; i++) {
+    if(authors[i] == req.query.author){
+      console.log("Author: " + authors[i] + " ISBN: " + isbns[i] + " Book: " + books[i] + " Date: " + dates[i]);
+    }
+  }
+
+  res.send("");
+
 });
 
 app.listen(3000, () => {
