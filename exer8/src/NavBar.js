7d05ea61e558628cbf26346b7418041fc9b79e32
Angelo Viloria <lpviloria@up.edu.ph>
2024-04-19 21:37:39 +0800

    feat: added NavBar.js for the navigation bar

---
+++ exer8/src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar(){
+      
+    return (
+        <div>
+            <div class="topnav">
+                <a>Lazado</a>
+                <a id = "apps" href="#appliances">Appliances</a>
+                <a id= "gadgets" href="#gadgets">Gadgets</a>
+                <a id = "accs" href="#accessories">Accessories</a>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
