fc1a9eac3137951f8d224aa17541de201f7fd14b
Angelo Viloria <lpviloria@up.edu.ph>
2024-05-01 23:30:40 +0800

    fix: cart is now preserved whenever the user switches tabs

--- exer9/src/App.js
+++ exer9/src/App.js
@@ -1,14 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import Appliances from './pages/apps';
 import Gadgets from './pages/gadgets';
 import Accessories from './pages/accs';
 import {CartAdder} from './components/cart';
-import {useState} from 'react';
 import './styles.css';
 
 function App() {
   let Component
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || {}); // Utilize local storage to store the current state of cart so that it is preserved whenever the page is changed
+
+  useEffect(() => { // Update the local storage whenever the cart is updated
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
   switch(window.location.pathname){
     case "/":
       Component = Appliances
